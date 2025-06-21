fefddf212128159a95d160438441b1658425119c
Yuriy Yarish <124111477+Michael-Zhinchyn@users.noreply.github.com>
2025-06-21 17:31:13 -0500

    update libs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import logo from "../../assets/dark-color-horizontal-logo.svg";
+import { Link } from "react-router-dom";
 import { WelcomeHeader } from "./Header.styled";
 import { Nav } from "./Header.styled";
 import { StyledLink } from "./Header.styled";
@@ -7,7 +8,9 @@ function Header() {
   return (
     <WelcomeHeader>
       <div>
-        <img src={logo} alt="company logo" width={120} />
+        <Link to="/">
+          <img src={logo} alt="company logo" width={120} />
+        </Link>
       </div>
       <Nav>
         <StyledLink to="/">Home</StyledLink>
