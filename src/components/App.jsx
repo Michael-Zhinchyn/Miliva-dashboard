db928ac43660203cc8cf07d97805d96153b5ab98
Yuriy Yarish <124111477+Michael-Zhinchyn@users.noreply.github.com>
2025-07-24 10:55:52 -0500

    added header

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header.jsx/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to Miliva dashboard</p>
+    </div>
+  );
+};
+
+export default App;
